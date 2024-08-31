997ed398700f9cf095f680af79bb7713b455c6d7
Yegi Candra Monanza <candramonanzayegi@gmail.com>
2024-09-01 01:28:38 +0700

    herosection update

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,11 +9,10 @@ const HeroSection = () => {
         <div className="grid grid-cols-1 lg:grid-cols-12">
             <div className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-amber-600">Hello, I'm {""}</span>
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-amber-600">Hello, I&apos;m {""}</span>
                     <br />
                     <TypeAnimation
                         sequence={[
-                            // Same substring at the start will only be typed out once, initially
                             'Yegi',
                             1000, // wait 1s before replacing "Mice" with "Hamsters"
                             'Web Developer',
@@ -25,7 +24,6 @@ const HeroSection = () => {
                         ]}
                         wrapper="span"
                         speed={50}
-                        
                         repeat={Infinity}
                     />
                 </h1>
