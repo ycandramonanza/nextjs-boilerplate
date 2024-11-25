514b704d2b69b1943968b0eb1ca36fedd5b10dc6
Yegi Candra Monanza <candramonanzayegi@gmail.com>
2024-11-25 16:50:00 +0700

    git push -u origin dev

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectSection from "./components/ProjectSection";
+import EmailSection from "./components/EmailSection";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
+        <ProjectSection />
+        <EmailSection />
       </div>
     </main>
   );
