2498c14b3172a89406d68f6f4df11a087b6cae93
Yegi Candra Monanza <candramonanzayegi@gmail.com>
2024-11-25 16:56:42 +0700

    fix

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -9,7 +9,7 @@ const EmailSection = () => {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-12 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-amber-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">Lets Connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {""}
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat,
